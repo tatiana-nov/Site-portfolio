5f8abba5052514499855fd398911741637497fec
Tatiana <sandynayms@yandex.ru>
2023-01-29 20:23:51 +0100

    Cite-portfolio

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const btnDarkMode=document.querySelector('.dark-mode-btn');
+ 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+}
+
+if (localStorage.getItem('darkMode')==='dark'){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+} else if (localStorage.getItem("darkMode")==="Light") {
+    btnDarkMode.classList.remove('dark-mode-btn--active');
+    document.body.classList.remove('dark');
+}
+
+
+
+window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change',()=>{
+    const newColorScheme= event.matches ? 'dark' : 'Light';
+    alert('Change!');
+    if (newColorScheme==='dark'){
+        btnDarkMode.classList.add('dark-mode-btn--active');
+        document.body.classList.add('dark');
+    } else {
+        btnDarkMode.classList.remove('dark-mode-btn--active');
+        document.body.classList.remove('dark')
+    }
+})
+
+btnDarkMode.onclick=function(){
+    btnDarkMode.classList.toggle('dark-mode-btn--active');
+    const isDark=document.body.classList.toggle('dark');
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'Light')
+    }
+}
